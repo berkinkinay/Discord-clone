e15bcd6ec980bff506f73ee2b43951ce9930368d
Berkin Kınay <103454964+developedbyven@users.noreply.github.com>
2022-11-04 23:06:27 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ const plugin = require('tailwindcss/plugin')
 module.exports = {
   plugins: [
     plugin(function({ addUtilities, addComponents, e, config }) {
-      // Add your custom styles here
+      
     }),
   ]
 }
